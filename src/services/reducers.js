0cdf69321597e7f24c0a2716b53f838c5a8486c9
rajan <rajanvasani9@gmail.com>
2022-09-12 15:54:05 +0530

    active portfolio reducer added to reducer list

--- src/services/reducers.js
+++ src/services/reducers.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import portfolios from './portfolios/portfoliosReducer';
 import portfolioDetails from './portfolios/portfolioDetailsReducer';
-
+import activePortfolio from './portfolios/activePortfolioReducer';
 export default combineReducers({
     portfolios,
-    portfolioDetails
+    portfolioDetails,
+    activePortfolio
 })
